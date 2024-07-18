f61d872890cd894ecb139de989c2f3eb1acb8ce4
Nick Thomas <nichoth@nichoth.com>
2024-07-18 13:31:37 -0700

    fix test

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,7 @@ import { attributesToString } from '../src/index.js'
 
 test('example', async t => {
     const el = document.querySelector('input')
-    const str = attributesToString(el!)
+    const str = attributesToString(Array.from(el!.attributes))
     t.equal(str, 'type="text" required name="fooo"',
         'should return a string in the right format')
 })
