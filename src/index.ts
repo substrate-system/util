1810cbd1f4a3f122d8aa233e1c67ae906e4051ad
Nick Thomas <nichoth@nichoth.com>
2024-07-18 14:31:27 -0700

    add object parse + serialize

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,26 @@
  * @returns {string} The attributes as a string like `key=val`
  */
 export function attributesToString (attrs:Attr[]):string {
-    return attrs.map(attr => attr.name + (attr.value === '' ? '' : '=' +
-        `"${attr.value}"`))
+    return attrs.map(attr => attr.name + (attr.value === '' ?
+        '' :
+        `="${attr.value}"`))
         .join(' ')
 }
 
+export function attributesAsObject (attrs:Attr[]):Record<string, string|true> {
+    return attrs.reduce((acc, attr) => {
+        acc[attr.name] = attr.value || true
+        return acc
+    }, {})
+}
+
+export function objectToString (obj:Record<string, string|true>):string {
+    return Object.keys(obj).map(k => {
+        return k + (obj[k] === true ? '' : `="${obj[k]}"`)
+    }).join(' ')
+}
+
 export default {
-    attributesToString
+    attributesToString,
+    attributesAsObject
 }
--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,30 @@
 import { test } from '@bicycle-codes/tapzero'
-import { attributesToString } from '../src/index.js'
+import {
+    attributesToString,
+    attributesAsObject,
+    objectToString
+} from '../src/index.js'
 
-test('attributesToString', async t => {
+test('attributesToString', t => {
     const el = document.querySelector('input')
     const str = attributesToString(Array.from(el!.attributes))
-    t.equal(str, 'type="text" required name="fooo"',
+    t.equal(str, 'type="text" required name="fooo" foo="bar"',
         'should return a string in the right format')
+})
+
+let obj
+test('attributesAsObject', t => {
+    const el = document.querySelector('input')
+    const attrs = Array.from(el!.attributes)
+    obj = attributesAsObject(attrs)
+    t.equal(obj.required, true, 'boolean attributes are set to `true`')
+    t.equal(obj.name, 'fooo', 'should have name attribute')
+    t.equal(obj.type, 'text', 'should have type attributes')
+    t.equal(obj.foo, 'bar', 'should parse arbitrary attributes')
+})
+
+test('object to string', t => {
+    const str = objectToString(obj)
+    t.equal(str, 'type="text" required name="fooo" foo="bar"',
+        'should serialize the attribute object')
 })
