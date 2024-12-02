e3d350085dfaab3ee8c58553156cfc3c4442c310
nichoth <nichoth@nichoth.com>
2024-12-02 14:08:20 -0800

    add bytes

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -1,3 +1,8 @@
 export const EM_DASH = '\u2014'
 export const EN_DASH = '\u2013'
 export const NBSP = '\u00A0'
+export const PETABYTE = (2 ^ 50)
+export const TERABYTE = (2 ^ 40)
+export const GIGABYTE = (2 ^ 30)
+export const MEGABYTE = (2 ^ 20)
+export const KILOBYTE = (2 ^ 10)
