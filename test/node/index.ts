2f63ed59ae35e2ecc1fa2a1a70d6a666cd964779
nichoth <nichoth@nichoth.com>
2025-06-11 17:28:39 -0700

    add a to string function

--- test/node/index.ts
+++ test/node/index.ts
@@ -1,6 +1,17 @@
 import { test } from '@substrate-system/tapzero'
+import { attributes } from '../../src/to-string.js'
 import '../../src/node/self.js'
 
 test('self exists', t => {
     t.ok(self, 'self should exist')
+})
+
+test('Create attributes string', t => {
+    const string = attributes({ hello: 'abc', ok: '123' })
+    t.equal(string, 'hello="abc" ok="123"')
+})
+
+test('Attributes can handle arrays', t => {
+    const string = attributes({ hello: 'ok', abc: ['aaa', 'bbb', 123] })
+    t.equal(string, 'hello="ok" abc="aaa bbb 123"', 'should convert the array')
 })
